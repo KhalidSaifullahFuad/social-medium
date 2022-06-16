db95b0304150e72364ec2074bdb46d070d62a924
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-06-17 00:11:10 +0600

    📦 Adder new Role attribute for User entity

--- src/main/java/com/fuad/entity/User.java
+++ src/main/java/com/fuad/entity/User.java
@@ -1,9 +1,15 @@
 package com.fuad.entity;
 
+import com.fuad.enums.Role;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -25,6 +31,11 @@ public class User implements Serializable {
     @Column(name = "email")
     private String email;
 
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "location_id", referencedColumnName = "id")
     private Location location;
@@ -35,4 +46,39 @@ public class User implements Serializable {
 
     @OneToMany(orphanRemoval = true, mappedBy = "user")
     private List<Status> statusList;
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//
+//        for(Role role : Role.values()) {
+//            authorities.add(() -> role.name());
+//        }
+//        return authorities;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
 }
