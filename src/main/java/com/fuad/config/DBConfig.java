95679a55aea354e17dc7edb6aa319d4f9181a847
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-06-20 18:46:48 +0600

    ✅ Created Root config and added necessary beans

--- src/main/java/com/fuad/config/DBConfig.java
+++ src/main/java/com/fuad/config/DBConfig.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan(basePackages = {"com.fuad.dao"})
 public class DBConfig {
 
     @Autowired
---
+++ src/main/java/com/fuad/config/RootConfig.java
@@ -0,0 +1,22 @@
+package com.fuad.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@ComponentScan(basePackages = {"com.fuad.service", "com.fuad.dao"})
+public class RootConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthSuccessHandler authSuccessHandler() {
+        return new AuthSuccessHandler();
+    }
+}
